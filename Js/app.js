ece992b9db512b5c45bf2ae5235239e22d0fd90d
Sami Adli <samiadli00@gmail.com>
2024-07-16 21:20:52 -0700

    Added new overlay menu

--- Js/app.js
+++ Js/app.js
@@ -1,9 +1,14 @@
-function ShowHide(x) {
-    x.classList.toggle("change");
-    var menu = document.getElementById("nav-list");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
+// function ShowHide(x) {
+//     x.classList.toggle("change");
+//     var menu = document.getElementById("nav-list");
+//     if (menu.style.display === "block") {
+//       menu.style.display = "none";
+//     } else {
+//       menu.style.display = "block";
+//     }
+//   }
+
+function toggleMenu(burger) {
+  burger.classList.toggle("change");
+  document.querySelector(".nav-list").classList.toggle("show-nav");
+}
